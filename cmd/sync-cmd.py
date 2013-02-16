ac94baa806a7cdd8d235182714336f6a22b74a6c
Matthieu Rakotojaona <matthieu.rakotojaona@gmail.com>
2013-02-16 19:12:32 +0100

    Frame messages
    Messages are now framed and sent in bulk.
    
    This is the format of a message:
    <length><header>\n<content>\0
    
    where length is the length of the message : <header>\n<content>.
    '\0' serves as a message delimiter.
    
    Also, display the upload speed of all the content.

--- cmd/sync-cmd.py
+++ cmd/sync-cmd.py
@@ -8,11 +8,7 @@ from twisted.internet import reactor
 
 from collections import deque
 
-### Protocol Implementation
-
-end="Bye-bye!"
-begin="REF"
-receive="REC"
+MAX_FRAME_SIZE = 40000
 
 _cp = None
 def cp():
@@ -31,86 +27,142 @@ def treat_refs(data):
     missing = []
     for line in data.split('\n'):
         (name, sha) = line.split(' ')
-        if not packList.exists(sha.decode('hex')):
-            missing.append(sha.decode('hex'))
+        if not packList.exists(sha):
+            missing.append(sha)
     return missing
 
-def want_next_missing():
-    if len(local_missing) > 0:
-        return 'WANT %s' % local_missing.popleft()
-    else:
-        return
+def decode_have(message):
+    hash, rem = message[:20], message[21:]
+    type = ''
+    while type not in ('commit', 'tree', 'blob'):
+        type += rem[0]
+        rem = rem[1:]
+    rem = rem[1:] # discard the '\n'
+    return hash, type, rem
 
-local_missing = deque()
 total_size_exchanged = 0
 beginning = time.time()
-def process_data(data):
-    global local_missing
+blobs_exchanged = 0
+def prepare_next_messages():
     global total_size_exchanged
     global beginning
-    cmd, message = data[0:4], data[5:]
-    if cmd == 'REFS':
-        missing = treat_refs(message)
-        print "missing %s commits" % len(missing)
+    global blobs_exchanged
 
-        local_missing.extend(missing)
-        return want_next_missing()
+    while len(local_missing) > 0 or len(remote_missing) > 0:
+        next_messages = []
+        total_size = 0
 
-    elif cmd == 'HAVE':
-        splitted = message.split('\n')
-        hash, type = splitted[0].split(' ')
-        if type == 'commit':
-            # firstline of the commit message contains the tree
-            tree_sha = splitted[1].split(' ')[1].decode('hex')
-            if not packList.exists(tree_sha):
-                local_missing.append(tree_sha)
+        while len(local_missing) > 0:
+            maybe_next = local_missing[0]
+            message = 'WANT %s' % maybe_next
+            message = struct.pack("!I", len(message)) + message + '\0'
+
+            if total_size + len(message) < MAX_FRAME_SIZE:
+                local_missing.popleft()
+                total_size += len(message)
+                next_messages.append(message)
             else:
-                print "I already have", '\n'.join(splitted[1:])
-        elif type == 'tree':
-            # each line contains an object
-            for (mode, name, hash) in git.tree_decode('\n'.join(splitted[1:])):
-                if not packList.exists(hash):
-                    local_missing.append(hash)
+                break
 
-        elif type == 'blob':
-            total_size_exchanged += len(message)
-            duration = time.time() - beginning
-            speed = total_size_exchanged / (duration) / 1024
-            log("%s kbps\n" % str(speed))
-        return want_next_missing()
+        while len(remote_missing) > 0:
+            maybe_next_sha = remote_missing[0]
+            file = cp().get(maybe_next_sha.encode('hex'))
+            type = file.next()
+            content = ''.join(part for part in file)
+            message = "HAVE %s %s\n%s" % (maybe_next_sha, type, content)
+            message = struct.pack("!I", len(message)) + message + '\0'
 
-    elif cmd == 'WANT':
-        sha = message.encode('hex')
-        file = cp().get(sha)
-        type = file.next()
-        content = ''.join(part for part in file)
+            if total_size + len(message) < MAX_FRAME_SIZE or len(next_messages) == 0:
+                remote_missing.popleft()
+                next_messages.append(message)
+                total_size += len(message)
+            else:
+                break
 
-        return "HAVE %s %s\n%s" % (sha, type, content)
+        total_size_exchanged += total_size
+        duration = time.time() - beginning
+        speed = total_size_exchanged / (duration) / 1024
+        qprogress("%s kbps\n" % str(speed))
+
+        yield next_messages
+
+def decode(buf):
+    """yield each framed message in the raw datagram received"""
+    start = 0
+    while start < len(buf):
+        length = struct.unpack("!I", buf[start:start+4])[0]
+        cmd = buf[start+4:start+8]
+        end = start + 4 + length
+        message = buf[start+9:end]
+
+        assert(buf[end:end+1] == '\0')
+
+        yield cmd, message
+
+        start = end+1
+
+local_missing = deque()
+remote_missing = deque()
+
+def process_data(data):
+    global local_missing
+
+    for cmd, message in decode(data):
+
+        if cmd == 'REFS':
+            missing = treat_refs(message)
+            print "missing %s commits" % len(missing)
+
+            local_missing.extend(missing)
+
+        elif cmd == 'HAVE':
+            hash, type, content = decode_have(message)
+            if type == 'commit':
+                # firstline of the commit message contains the tree
+                tree_sha = content.split('\n')[0].split(' ')[1].decode('hex')
+                if not packList.exists(tree_sha):
+                    local_missing.append(tree_sha)
+            elif type == 'tree':
+                # each line contains an object
+                for (mode, name, hash) in git.tree_decode(content):
+                    if not packList.exists(hash):
+                        local_missing.append(hash)
+
+            elif type == 'blob':
+                pass
+                #print "blob"
+
+            else:
+                print "wrong type:", type
+
+        elif cmd == 'WANT':
+            remote_missing.append(message)
 
 class ContentServer(Int32StringReceiver):
 
     def stringReceived(self, data):
-        next_message = process_data(data)
-        if next_message:
-            self.sendString(next_message)
-        else:
-            self.transport.loseConnection()
+        process_data(data)
+        for next_messages in prepare_next_messages():
+            if len(next_messages) > 0:
+                tosend = ''.join(next_messages)
+                self.sendString(tosend)
 
 class ContentClient(Int32StringReceiver):
 
     def connectionMade(self):
-
         allrefs = []
         for (refname, sha) in git.list_refs():
-            allrefs.append(refname + ' ' + sha.encode('hex'))
-        self.sendString('REFS\n' + '\n'.join(allrefs))
+            allrefs.append(refname + ' ' + sha)
+        message = 'REFS\n' + '\n'.join(allrefs)
+        message = struct.pack("!I", len(message)) + message + '\0'
+        self.sendString(message)
 
     def stringReceived(self, data):
-        next_message = process_data(data)
-        if next_message:
-            self.sendString(next_message)
-        else:
-            self.transport.loseConnection()
+        process_data(data)
+        for next_messages in prepare_next_messages():
+            if len(next_messages) > 0:
+                tosend = ''.join(next_messages)
+                self.sendString(tosend)
 
 
 class ContentClientFactory(ClientFactory):
